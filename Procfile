85140576a2db25c2d93f69322a0dfbef2394d563
Kazon Wilson <kazonwilson@gmail.com>
2020-06-26 00:06:27 -0700

    Switch to redis queue for job (#18)
    * Switch to redis queue for job
    
    * remove debug stuff
    
    * Update jobs.py
    
    * Create app.json
    
    * Update jobs.py
    
    * Add db and user context managers, increase job timeout duration
    
    * Update jobs.py
    
    * Update jobs.py
    
    * Remove huey

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: uvicorn --host 0.0.0.0 --port $PORT run:app
-worker: huey_consumer.py threadback.jobs.jobs.huey -w 1 -k process -C
+worker: python -u run-worker.py
--- threadback/app.py
+++ threadback/app.py
@@ -1,7 +1,8 @@
 import importlib
+import urllib.parse
 
-from huey import RedisHuey
 from mongoengine import connect, disconnect
+from redis import Redis
 from starlette.applications import Starlette
 from starlette.middleware.cors import CORSMiddleware
 from starlette.middleware.trustedhost import TrustedHostMiddleware
@@ -18,18 +19,9 @@ def disconnect_from_db():
     disconnect(settings.DB_NAME)
 
 
-huey = RedisHuey("jobs", url=settings.REDIS_URL, blocking=True, utc=False)
-
-
-@huey.on_startup()
-def _connect_to_db():
-    connect(settings.DB_NAME, host=settings.MONGODB_URI)
-
-
-@huey.on_shutdown()
-def _disconnect_from_db():
-    disconnect(settings.DB_NAME)
-
+urllib.parse.uses_netloc.append("redis")
+url = urllib.parse.urlparse(settings.REDIS_URL)
+conn = Redis(host=url.hostname, port=url.port, db=0, password=url.password)
 
 app = Starlette(
     debug=settings.DEBUG, on_startup=[connect_to_db], on_shutdown=[disconnect_from_db],
